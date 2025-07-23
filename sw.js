5cfa6ce24765595a0b685039084eb1bb5a8713a5
Onique-Unique <kiddohbeats@yahoo.com>
2025-07-23 10:10:22 -0400

    Content App Update

--- sw.js
+++ sw.js
@@ -1,23 +1,48 @@
-self.addEventListener("install", (event) => {
+const CACHE_NAME = 'cruisesnitch-cache-v2'; // Bump this when you update your files
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/manifest.webmanifest',
+  '/css/portNav.css',
+  '/js/portNav.js'
+];
+
+// Cache core assets on install
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open("cruisesnitch-cache").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/icons/icon-192.png",
-        "/icons/icon-512.png",
-        "/manifest.webmanifest",
-        "/css/portNav.css", // Add your CSS path
-        "/js/portNav.js" // Add your JS path
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
     })
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+// Clean up old caches on activate
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
+});
+
+// Serve from cache, fall back to network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
+
+// Communicate update to the client
+self.addEventListener('message', (event) => {
+  if (event.data === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
